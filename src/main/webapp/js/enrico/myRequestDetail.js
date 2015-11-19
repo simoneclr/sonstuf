8d74108a79dca94ba4a1e9ffc0e8bfb21dcc8188
EnricoMagnago <en.magnago@gmail.com>
2015-11-19 11:12:51 +0100

    manca ancora stato 3 di myRequestDetail

---
+++ src/main/webapp/js/enrico/myRequestDetail.js
@@ -0,0 +1,83 @@
+function addOffert(id) {
+	var source = $("#offer").html();
+	var template = Handlebars.compile(source);
+	var context = {
+		OFFER_OFFERERNAME: "I'mOfferer",
+		OFFER_ID: id,
+		REQUESTER_NAME: "I'mRequester"
+	};
+	var offerHtml = template(context);
+	$(".inCorso").append(offerHtml);
+
+
+	$("#offererRank" + id).rating({
+		'showCaption': false,
+		'showClear': false,
+		'readonly': true
+	}).rating('update', 5);
+
+}
+function prepareState0() {
+	addOffert(1);
+	addOffert(2);
+	$(".inCorso").removeClass("hidden");
+}
+
+function prepareState1() {
+	var source = $("#waitingValuation").html();
+	var template = Handlebars.compile(source);
+	var context = {
+	DELEGATE_NAME: "I'mDelegate"
+	};
+	var offerHtml = template(context);
+	$(".inAttesaDiValutazione").empty().append(offerHtml);
+	$("#valuationRank").rating({
+		'showCaption': false,
+		'showClear': false,
+		'readonly': false
+	}).rating('update', 5);
+	$(".inAttesaDiValutazione").removeClass("hidden");
+}
+
+function prepareState2() {
+
+	$(".done").removeClass("hidden");
+}
+function modifyState(state) {
+	switch (state) {
+		case 0:
+			prepareState0();
+			break;
+		case 1:
+			prepareState1();
+			break;
+		case 2:
+			prepareState2();
+			break;
+		case 3:
+			alert("sorry invalid state");
+	}
+}
+
+$(document).ready(function () {
+	var source = $("#request").html();
+	var template = Handlebars.compile(source);
+	var context = {
+		REQUEST_IMAGE: "http://media.lavorincasa.it/blog/giardinaggio/2013/03/lasagna3.jpg",
+		REQUEST_TITLE: "Orto",
+		REQUEST_DESCRIPTION: "Qualcuno potrebbe aiutarmi a togliere le erbacce?",
+		PLACE: "Dambel",
+		TIME: "22/12/1994"
+	};
+	var requestHtml = template(context);
+	$("#requestContainer").empty().append(requestHtml);
+	$(".requesterName").empty().append("I'mRequester");
+	modifyState(1);
+
+	$("#rating-oDone").rating({
+		'showCaption': false,
+		'showClear': false,
+		'readonly': true
+	}).rating('update', 5);
+});
+
