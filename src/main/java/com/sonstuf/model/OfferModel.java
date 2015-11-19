b05c9245c84849ea97df2d5901a44d059513a6dd
Enrico Testori <hypertesto@gmail.com>
2015-11-19 10:43:31 +0100

    Aggiunto metodo getOfferByUser

--- src/main/java/com/sonstuf/model/OfferModel.java
+++ src/main/java/com/sonstuf/model/OfferModel.java
@@ -1,6 +1,7 @@
 package com.sonstuf.model;
 
 import com.sonstuf.model.bean.Offer;
+import com.sonstuf.model.bean.User;
 
 import javax.naming.NamingException;
 import java.sql.Connection;
@@ -28,6 +29,48 @@ public class OfferModel {
 
 		ps = connection.prepareStatement(query);
 		ps.setInt( 1, id );
+
+		rs = ps.executeQuery();
+
+		if ( rs.next() ) {
+
+			res = new Offer();
+			res.setIdOffer( rs.getInt("idoffer"));
+			res.setInCharge( rs.getBoolean("isincharge"));
+			res.setIdRequest( rs.getInt("idrequest"));
+			res.setIdUser( rs.getInt("iduser"));
+			res.setStatus( rs.getInt("status"));
+			res.setPostTime( rs.getTimestamp("posttime"));
+
+		} else {
+
+			res = null;
+
+		}
+
+		rs.close();
+		ps.close();
+		connection.close();
+
+		return res;
+
+	}
+
+	public static Offer getOfferByUser ( User u ) throws SQLException, NamingException {
+
+		Connection connection;
+		PreparedStatement ps;
+		ResultSet rs;
+		Offer res;
+
+
+		String query = 	"SELECT * from offer\n" +
+						"WHERE idoffer = ? ;";
+
+		connection = Connector.getConnection();
+
+		ps = connection.prepareStatement(query);
+		ps.setInt( 1, u.getIdUser() );
 
 		rs = ps.executeQuery();
 
