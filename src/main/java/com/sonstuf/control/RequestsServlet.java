a182b985b850e7269684aace743293bf82ad886c
Alberto Bonizzi <aseoista@gmail.com>
2015-11-19 15:53:36 +0100

    Finished servlet

--- src/main/java/com/sonstuf/control/RequestsServlet.java
+++ src/main/java/com/sonstuf/control/RequestsServlet.java
@@ -2,6 +2,7 @@ package com.sonstuf.control;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -85,17 +86,13 @@ public class RequestsServlet extends HttpServlet {
 			
 			for (Request request : requests) {
 				
-				System.out.println (request.getIdRequest ());
-				
 				if (!done) {
 					done = true;
 				} else {
 					writer.write (',');
 				}
 				
-				temp = MiniPacket.requestToMiniPacket (request);
-				
-				System.out.println ("temp:" + mapper.writeValueAsString (temp));
+				temp = requestToMiniPacket (request);
 				
 				if (temp != null) {
 					
@@ -105,70 +102,177 @@ public class RequestsServlet extends HttpServlet {
 		
 		} catch (SQLException | NamingException | JsonProcessingException e) {
 			
+			e.printStackTrace ();
 		}
 		
 		writer.write (']');
 	}
 	
-	private static class MiniPacket {
+	private MiniUser userToMiniUser (User user) {
 		
-		public int idRequest;
-		public MiniUser user;
-		public MiniRequest request;
+		MiniUser res;
 		
-		public static MiniPacket requestToMiniPacket (Request request) {
+		res = new MiniUser ();
+		
+		res.name = user.getName ();
+		res.rankR = user.getRankP ();
+		
+		return res;
+	}
+	
+	public MiniPacket requestToMiniPacket (Request request) {
+		
+		MiniPacket res;
+		
+		try {
 			
-			MiniPacket res;
+			res = new MiniPacket ();
 			
-			try {
-				
-				res = new MiniPacket ();
-				
-				res.idRequest = request.getIdRequest ();
-				res.user = MiniUser.userToMiniUser (
-						UserModel.getUserById (request.getIdUser ()));
-				res.request = new MiniRequest ();
-				res.request.category = CategoryModel
-						.getCategoryById (request.getIdCategory ()).getName ();
-				res.request.place = request.getPlace ();
-				res.request.time = request.getDateTime ();
-				res.request.postTimestamp = request.getPostTime ().toString ();
+			res.idRequest = request.getIdRequest ();
+			res.user = userToMiniUser (
+					UserModel.getUserById (request.getIdUser ()));
+			res.request = new MiniRequest ();
+			res.request.category = CategoryModel
+					.getCategoryById (request.getIdCategory ()).getName ();
+			res.request.place = request.getPlace ();
+			res.request.time = request.getDateTime ();
+			res.request.postTimestamp = request.getPostTime ().toString ();
 			
-			} catch (SQLException | NamingException e) {
-				
-				e.printStackTrace ();
-				res = null;
-			}
+		} catch (SQLException | NamingException e) {
 			
-			
-			return res;
+			e.printStackTrace ();
+			res = null;
+		}
+		
+		return res;
+	}
+	
+	private class MiniPacket {
+		
+		private int idRequest;
+		private MiniUser user;
+		private MiniRequest request;
+		/**
+		 * @return the idRequest
+		 */
+		public int getIdRequest () {
+			return idRequest;
+		}
+		/**
+		 * @param idRequest the idRequest to set
+		 */
+		public void setIdRequest (int idRequest) {
+			this.idRequest = idRequest;
+		}
+		/**
+		 * @return the user
+		 */
+		public MiniUser getUser () {
+			return user;
+		}
+		/**
+		 * @param user the user to set
+		 */
+		public void setUser (MiniUser user) {
+			this.user = user;
+		}
+		/**
+		 * @return the request
+		 */
+		public MiniRequest getRequest () {
+			return request;
+		}
+		/**
+		 * @param request the request to set
+		 */
+		public void setRequest (MiniRequest request) {
+			this.request = request;
 		}
 	}
 	
-	private static class MiniUser {
+	private class MiniUser {
 		
-		private String name;
-		private double rankR;
-		
-		private static MiniUser userToMiniUser (User user) {
-			
-			MiniUser res;
-			
-			res = new MiniUser ();
-			
-			res.name = user.getName ();
-			res.rankR = user.getRankP ();
-			
-			return res;
+		private String name  = "ciao";
+		private double rankR = 5.4;
+		/**
+		 * @return the name
+		 */
+		public String getName () {
+			return name;
+		}
+		/**
+		 * @param name the name to set
+		 */
+		public void setName (String name) {
+			this.name = name;
+		}
+		/**
+		 * @return the rankR
+		 */
+		public double getRankR () {
+			return rankR;
+		}
+		/**
+		 * @param rankR the rankR to set
+		 */
+		public void setRankR (double rankR) {
+			this.rankR = rankR;
 		}
 	}
 	
-	private static class MiniRequest {
+	private class MiniRequest {
 		
 		private String category;
 		private String place;
 		private String time;
 		private String postTimestamp;
+		/**
+		 * @return the category
+		 */
+		public String getCategory () {
+			return category;
+		}
+		/**
+		 * @param category the category to set
+		 */
+		public void setCategory (String category) {
+			this.category = category;
+		}
+		/**
+		 * @return the place
+		 */
+		public String getPlace () {
+			return place;
+		}
+		/**
+		 * @param place the place to set
+		 */
+		public void setPlace (String place) {
+			this.place = place;
+		}
+		/**
+		 * @return the time
+		 */
+		public String getTime () {
+			return time;
+		}
+		/**
+		 * @param time the time to set
+		 */
+		public void setTime (String time) {
+			this.time = time;
+		}
+		/**
+		 * @return the postTimestamp
+		 */
+		public String getPostTimestamp () {
+			return postTimestamp;
+		}
+		/**
+		 * @param postTimestamp the postTimestamp to set
+		 */
+		public void setPostTimestamp (String postTimestamp) {
+			this.postTimestamp = postTimestamp;
+		}
 	}
-	
 }
