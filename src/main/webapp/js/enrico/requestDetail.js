b1ed9065ec883c26e305453c5c0c5187928d068d
EnricoMagnago <en.magnago@gmail.com>
2015-11-19 08:46:09 +0100

    requestDetail ready to load content from db

---
+++ src/main/webapp/js/enrico/requestDetail.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+	var source = $("#request").html();
+	var template = Handlebars.compile(source);
+	var context = {
+		REQUEST_IMAGE: "http://media.lavorincasa.it/blog/giardinaggio/2013/03/lasagna3.jpg",
+		REQUEST_TITLE: "Orto",
+		REQUEST_DESCRIPTION: "Cerco una persona che possa aiutarmi a togliere le erbacce dall'orto",
+		PLACE: "Dambel",
+		TIME: "22/12/1994"
+	};
+	var requestHtml = template(context);
+	$("#requestContainer").empty().append(requestHtml);
+	$(".requesterName").empty().append("I'mRequester");
+
+	$("#rating-o").rating({
+		'showCaption': false,
+		'showClear': false,
+		'readonly': true
+	}).rating('update', 5);
+});
