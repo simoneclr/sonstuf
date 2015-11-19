bfcc6cd23bfd2dc2d06e5f0909039543681c69e5
EnricoMagnago <en.magnago@gmail.com>
2015-11-19 16:18:57 +0100

    feat: loading from back-end by ajax done

--- src/main/webapp/js/enrico/myOfferDetail.js
+++ src/main/webapp/js/enrico/myOfferDetail.js
@@ -1,13 +1,13 @@
-function prepareState0() {
-	$(".userName").empty().append("userName");
+function prepareState0(requestingUser) {
+	$(".userName").empty().append(requestingUser.name);
 	$(".inCorso").removeClass("hidden");
 }
 
-function prepareState1() {
+function prepareState1(requestingUser) {
 	var source = $("#waitingValuation").html();
 	var template = Handlebars.compile(source);
 	var context = {
-		REQUESTER_NAME: "I'mRequester"
+		REQUESTER_NAME: requestingUser.name
 	};
 	var offerHtml = template(context);
 	$(".inAttesaDiValutazione").empty().append(offerHtml);
@@ -16,16 +16,16 @@ function prepareState1() {
 		'showCaption': false,
 		'showClear': false,
 		'readonly': false
-	}).rating('update', 5);
+	}).rating('update', requestingUser.rankR);
 	$(".inAttesaDiValutazione").removeClass("hidden");
 }
 
-function prepareState2() {
+function prepareState2(requestingUser, valuation) {
 	var source = $("#doneValuation").html();
 	var template = Handlebars.compile(source);
 	var context = {
-		REQUESTER_NAME: "I'mRequester",
-		VALUATION_COMMENT: "è stato molto preciso e gentile. Non ha preteso l'impossibile"
+		REQUESTER_NAME: requestingUser.name,
+		VALUATION_COMMENT: valuation.comment
 	};
 	var valuationHtml = template(context);
 	$(".done").empty().append(valuationHtml);
@@ -33,38 +33,44 @@ function prepareState2() {
 		'showCaption': false,
 		'showClear': false,
 		'readonly': true
-	}).rating('update', 5);
+	}).rating('update', parseInt(valuation.rank));
 	$(".done").removeClass("hidden");
 }
-function setState(state) {
+function setState(state, requestingUser, valuation) {
 	switch (state) {
 		case 0:
-			prepareState0();
+			prepareState0(requestingUser);
 			break;
 		case 1:
-			prepareState1();
+			prepareState1(requestingUser);
 			break;
 		case 2:
-			prepareState2();
+			prepareState2(requestingUser, valuation);
 			break;
 		case 3:
 			alert("sorry invalid state");
 	}
 }
+var idOffer = '${idOffer}';
+$.getJSON("/getmyoffer?idOffer=" + idOffer, function (json) {
+	var request = json.request;
+	var state = parseInt(json.state);
+	var requestingUser = json.requestingUser;
+	var valuation = json.valuation;
+	$(document).ready(function () {
+		var source = $("#request").html();
+		var template = Handlebars.compile(source);
+		var context = {
+			REQUEST_IMAGE: request.photo,
+			REQUEST_TITLE: request.title,
+			REQUEST_DESCRIPTION: request.description,
+			PLACE: request.place,
+			TIME: request.time
+		};
+		var requestHtml = template(context);
+		$("#requestContainer").empty().append(requestHtml);
 
-$(document).ready(function () {
-	var source = $("#request").html();
-	var template = Handlebars.compile(source);
-	var context = {
-		REQUEST_IMAGE: "http://media.lavorincasa.it/blog/giardinaggio/2013/03/lasagna3.jpg",
-		REQUEST_TITLE: "Orto",
-		REQUEST_DESCRIPTION: "Cerco una persona che possa aiutarmi a togliere le erbacce dall'orto",
-		PLACE: "Dambel",
-		TIME: "22/12/1994"
-	};
-	var requestHtml = template(context);
-	$("#requestContainer").empty().append(requestHtml);
+		setState(state, requestingUser, valuation);
 
-	setState(2);
-
+	});
 });
