ed36ec0c911125f66962e231d1cb2983dec0d49d
Enrico Testori <hypertesto@gmail.com>
2015-11-19 10:36:49 +0100

    Sistemate migrations

--- src/main/resources/db/migration/004__populate_requests.sql
+++
@@ -1,7 +0,0 @@
-/*
--- Query: SELECT * FROM sonstuf.request
-LIMIT 0, 1000
-
--- Date: 2015-11-17 10:25
-*/
-INSERT INTO `request` (`idrequest`,`title`,`description`,`place`,`datetime`,`photo`,`iduser`,`idcategory`,`status`) VALUES (2,'Sfalcio del mio prato','Ho bisogno di una mano a sfalciare il prato davanti casa. Il prato è grande poco più di due utilitarie.','Falcade','Metà mattinata durante la settimana','',2,3,0);
