2a7a183d799c7ed4d686df73ddaf82731c21573b
EnricoMagnago <en.magnago@gmail.com>
2015-11-19 15:29:02 +0100

    myRequestDetail e reqeustDetail load data from back-end, needs testing

--- src/main/webapp/js/enrico/requestDetail.js
+++ src/main/webapp/js/enrico/requestDetail.js
@@ -1,20 +1,26 @@
-$(document).ready(function () {
-	var source = $("#request").html();
-	var template = Handlebars.compile(source);
-	var context = {
-		REQUEST_IMAGE: "http://media.lavorincasa.it/blog/giardinaggio/2013/03/lasagna3.jpg",
-		REQUEST_TITLE: "Orto",
-		REQUEST_DESCRIPTION: "Cerco una persona che possa aiutarmi a togliere le erbacce dall'orto",
-		PLACE: "Dambel",
-		TIME: "22/12/1994"
-	};
-	var requestHtml = template(context);
-	$("#requestContainer").empty().append(requestHtml);
-	$(".requesterName").empty().append("I'mRequester");
+var idRequest = '${idRequest}';
+$.getJSON("/getrequest?idRequest=" + idRequest, function (json) {
+	var request = json.request;
+	$(document).ready(function () {
+		var source = $("#request").html();
+		var template = Handlebars.compile(source);
+		var context = {
+			REQUEST_IMAGE: request.photo,
+			REQUEST_TITLE: request.title,
+			REQUEST_DESCRIPTION: request.description,
+			PLACE: request.place,
+			TIME: request.time
+		};
+		var requestHtml = template(context);
+		$("#requestContainer").empty().append(requestHtml);
+		$(".requesterName").empty().append(user.name);
 
-	$("#rating-o").rating({
-		'showCaption': false,
-		'showClear': false,
-		'readonly': true
-	}).rating('update', 5);
+		$("#rating-o").rating({
+			'showCaption': false,
+			'showClear': false,
+			'readonly': true
+		}).rating('update', parseInt(user.rankR));
+	});
 });
+var user = json.user;
+
