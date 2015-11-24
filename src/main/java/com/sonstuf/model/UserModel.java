1724f3b26715a00e366caf2e4fb162163b1b55c2
EnricoMagnago <en.magnago@gmail.com>
2015-11-24 15:45:09 +0100

    guardate se vi piace, in caso basta fare rollback

--- src/main/java/com/sonstuf/model/UserModel.java
+++ src/main/java/com/sonstuf/model/UserModel.java
@@ -5,21 +5,46 @@
  */
 package com.sonstuf.model;
 
+import com.sonstuf.model.bean.User;
+import com.sonstuf.utils.PasswordHash;
+import com.sonstuf.utils.Retval;
+
+import javax.naming.NamingException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import javax.naming.NamingException;
-
-import com.sonstuf.model.bean.User;
-import com.sonstuf.utils.PasswordHash;
-import com.sonstuf.utils.Retval;
 
 /**
  * @author hypertesto
  */
 public class UserModel {
+	private static List<User> executeQuery(PreparedStatement prepStatement) throws SQLException {
+		ResultSet rs = prepStatement.executeQuery();
+		List<User> res = new ArrayList<>();
+		try {
+			while (rs.next()) {
+				User user = new User();
+
+				user.setIdUser(rs.getInt("iduser"));
+				user.setName(rs.getString("name"));
+				user.setSurname(rs.getString("surname"));
+				user.setPhone(rs.getString("phone"));
+				user.setPasswordHash(rs.getString("passwordhash"));
+				user.setRankO(rs.getFloat("ranko"));
+				user.setRankP(rs.getFloat("rankp"));
+				user.setBirthDate(rs.getDate("birthdate"));
+				user.setAdmin(rs.getBoolean("admin"));
+
+				res.add(user);
+			}
+		} finally {
+			rs.close();
+			prepStatement.close();
+		}
+		return res;
+	}
 
 	/**
 	 * Inserts a user into the db if not already in.
@@ -97,45 +122,20 @@ public class UserModel {
 	 */
 	public static User getUserByMail(String email) throws SQLException, NamingException {
 		Connection connection;
-		PreparedStatement ps;
-		ResultSet rs;
-		User res;
 
 		String query = "SELECT * from user\n" +
 				"WHERE email = ? ;";
 
 		connection = Connector.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setString(1, email);
 
-		ps = connection.prepareStatement(query);
-		ps.setString(1, email);
-
-		rs = ps.executeQuery();
-
-		if (rs.next()) {
-
-			res = new User();
-
-			res.setIdUser(rs.getInt("iduser"));
-			res.setName(rs.getString("name"));
-			res.setSurname(rs.getString("surname"));
-			res.setPhone(rs.getString("phone"));
-			res.setPasswordHash(rs.getString("passwordhash"));
-			res.setRankO(rs.getFloat("ranko"));
-			res.setRankP(rs.getFloat("rankp"));
-			res.setBirthDate(rs.getDate("birthdate"));
-			res.setAdmin(rs.getBoolean("admin"));
-
-
-		} else {
-
-			res = null;
-		}
-
-		rs.close();
-		ps.close();
+		List<User> userList = executeQuery(preparedStatement);
 		connection.close();
-
-		return res;
+		if (userList.size() > 0)
+			return userList.get(0);
+		else //if it's empty
+			return null;
 	}
 
 	/**
@@ -149,9 +149,6 @@ public class UserModel {
 
 		Connection connection;
 		PreparedStatement ps;
-		ResultSet rs;
-		User res;
-
 
 		String query = "SELECT * from user\n" +
 				"WHERE phone = ? ;";
@@ -161,33 +158,13 @@ public class UserModel {
 		ps = connection.prepareStatement(query);
 		ps.setString(1, phone);
 
-		rs = ps.executeQuery();
+		List<User> userList = executeQuery(ps);
 
-		if (rs.next()) {
-
-			res = new User();
-
-			res.setIdUser(rs.getInt("iduser"));
-			res.setName(rs.getString("name"));
-			res.setSurname(rs.getString("surname"));
-			res.setPhone(rs.getString("phone"));
-			res.setPasswordHash(rs.getString("passwordhash"));
-			res.setRankO(rs.getFloat("ranko"));
-			res.setRankP(rs.getFloat("rankp"));
-			res.setBirthDate(rs.getDate("birthdate"));
-			res.setAdmin(rs.getBoolean("admin"));
-
-
-		} else {
-
-			res = null;
-		}
-
-		rs.close();
-		ps.close();
 		connection.close();
-
-		return res;
+		if (userList.size() > 0)
+			return userList.get(0);
+		else //if it's empty
+			return null;
 	}
 
 	public static User getUserById(int id) throws SQLException, NamingException {
@@ -205,41 +182,18 @@ public class UserModel {
 		ps = connection.prepareStatement(query);
 		ps.setInt(1, id);
 
-		rs = ps.executeQuery();
+		List<User> userList = executeQuery(ps);
 
-		if (rs.next()) {
-
-			res = new User();
-
-			res.setIdUser(rs.getInt("iduser"));
-			res.setName(rs.getString("name"));
-			res.setSurname(rs.getString("surname"));
-			res.setPhone(rs.getString("phone"));
-			res.setPasswordHash(rs.getString("passwordhash"));
-			res.setRankO(rs.getFloat("ranko"));
-			res.setRankP(rs.getFloat("rankp"));
-			res.setBirthDate(rs.getDate("birthdate"));
-			res.setAdmin(rs.getBoolean("admin"));
-
-
-		} else {
-
-			res = null;
-		}
-
-		rs.close();
-		ps.close();
 		connection.close();
-
-		return res;
-
+		if (userList.size() > 0)
+			return userList.get(0);
+		else //if it's empty
+			return null;
 	}
 
 	public static List<User> getUserByName(String name) throws SQLException, NamingException {
 		Connection connection;
 		PreparedStatement ps;
-		ResultSet rs;
-		List<User> res = new ArrayList<>();
 
 		String query = "SELECT * from user\n" +
 				"WHERE name = ? ;";
@@ -249,24 +203,11 @@ public class UserModel {
 		ps = connection.prepareStatement(query);
 		ps.setString(1, name);
 
-		rs = ps.executeQuery();
-		while (rs.next()) {
+		List<User> userList = executeQuery(ps);
 
-			User user = new User();
+		connection.close();
 
-			user.setIdUser(rs.getInt("iduser"));
-			user.setName(rs.getString("name"));
-			user.setSurname(rs.getString("surname"));
-			user.setPhone(rs.getString("phone"));
-			user.setPasswordHash(rs.getString("passwordhash"));
-			user.setRankO(rs.getFloat("ranko"));
-			user.setRankP(rs.getFloat("rankp"));
-			user.setBirthDate(rs.getDate("birthdate"));
-			user.setAdmin(rs.getBoolean("admin"));
-
-			res.add(user);
-		}
-		return res;
+		return userList;
 	}
 
 	public static List<User> getUserBySurname(String surname) throws SQLException, NamingException {
@@ -283,24 +224,11 @@ public class UserModel {
 		ps = connection.prepareStatement(query);
 		ps.setString(1, surname);
 
-		rs = ps.executeQuery();
-		while (rs.next()) {
+		List<User> userList = executeQuery(ps);
 
-			User user = new User();
+		connection.close();
 
-			user.setIdUser(rs.getInt("iduser"));
-			user.setName(rs.getString("name"));
-			user.setSurname(rs.getString("surname"));
-			user.setPhone(rs.getString("phone"));
-			user.setPasswordHash(rs.getString("passwordhash"));
-			user.setRankO(rs.getFloat("ranko"));
-			user.setRankP(rs.getFloat("rankp"));
-			user.setBirthDate(rs.getDate("birthdate"));
-			user.setAdmin(rs.getBoolean("admin"));
-
-			res.add(user);
-		}
-		return res;
+		return userList;
 	}
 
 	public static List<User> getUserByBirthdate(Date birthdate) throws SQLException, NamingException {
@@ -317,24 +245,11 @@ public class UserModel {
 		ps = connection.prepareStatement(query);
 		ps.setDate(1, birthdate);
 
-		rs = ps.executeQuery();
-		while (rs.next()) {
+		List<User> userList = executeQuery(ps);
 
-			User user = new User();
+		connection.close();
 
-			user.setIdUser(rs.getInt("iduser"));
-			user.setName(rs.getString("name"));
-			user.setSurname(rs.getString("surname"));
-			user.setPhone(rs.getString("phone"));
-			user.setPasswordHash(rs.getString("passwordhash"));
-			user.setRankO(rs.getFloat("ranko"));
-			user.setRankP(rs.getFloat("rankp"));
-			user.setBirthDate(rs.getDate("birthdate"));
-			user.setAdmin(rs.getBoolean("admin"));
-
-			res.add(user);
-		}
-		return res;
+		return userList;
 	}
 
 	public static List<User> getAllUsers() throws SQLException, NamingException {
@@ -349,24 +264,11 @@ public class UserModel {
 
 		ps = connection.prepareStatement(query);
 
-		rs = ps.executeQuery();
-		while (rs.next()) {
+		List<User> userList = executeQuery(ps);
 
-			User user = new User();
+		connection.close();
 
-			user.setIdUser(rs.getInt("iduser"));
-			user.setName(rs.getString("name"));
-			user.setSurname(rs.getString("surname"));
-			user.setPhone(rs.getString("phone"));
-			user.setPasswordHash(rs.getString("passwordhash"));
-			user.setRankO(rs.getFloat("ranko"));
-			user.setRankP(rs.getFloat("rankp"));
-			user.setBirthDate(rs.getDate("birthdate"));
-			user.setAdmin(rs.getBoolean("admin"));
-
-			res.add(user);
-		}
-		return res;
+		return userList;
 	}
 
 
