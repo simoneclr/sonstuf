579d954d027b09d9fe6f8f0fdfc3580c510cd647
Alberto Bonizzi <aseoista@gmail.com>
2015-11-26 17:42:47 +0100

    Added datetime format

--- src/main/java/com/sonstuf/utils/ProjectGlobals.java
+++ src/main/java/com/sonstuf/utils/ProjectGlobals.java
@@ -6,4 +6,5 @@ package com.sonstuf.utils;
 public class ProjectGlobals {
 	public static final String DATE_INPUT_FORMAT = "yyyy-MM-dd";
 	public static final String DATE_OUTPUT_FORMAT = "dd/MM/yyyy";
+	public static final String DATETIME_OUTPUT_FORMAT = "dd/MM/yyyy HH:mm";
 }
