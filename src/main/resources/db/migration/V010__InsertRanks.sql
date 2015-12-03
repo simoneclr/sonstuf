6d9891d61a6a179731d54bac0f863e6e720fe0b1
Enrico Testori <hypertesto@gmail.com>
2015-12-03 11:08:15 +0100

    Inserito rank su una richiesta

---
+++ src/main/resources/db/migration/V010__InsertRanks.sql
@@ -0,0 +1,2 @@
+INSERT INTO `sonstuf`.`requesterrank` (`rank`, `comment`, `idrequest`) VALUES ('4', 'Il proprietario è stato molto gentile e onesto, l\'unica cosa è che la sua attrezzatura non funzionava e ho dovuto prendere la mia', '2');
+
