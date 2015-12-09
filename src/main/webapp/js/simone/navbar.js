67ada2cae09629a70c7a786d34365615f10f33a9
EnricoMagnago <en.magnago@gmail.com>
2015-12-09 12:27:34 +0100

    logout + carino

--- src/main/webapp/js/simone/navbar.js
+++ src/main/webapp/js/simone/navbar.js
@@ -1,14 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	$.getJSON("/AuthenticationServlet?op=checkLogin", function(data){
-	}).done(function(data){
+	$.getJSON("/AuthenticationServlet?op=checkLogin", function (data) {
+	}).done(function (data) {
 		var success = data.success;
 
-		if (success){
+		if (success) {
 			$("#logged-user-nav").show();
 		} else {
 			$("#not-logged-user-nav").show();
 		}
-	})
+	});
+
+	$("#logout").on("click", function () {
+		$.getJSON("/AuthenticationServlet?op=logout", function (data) {
+		}).done(function (data) {
+			var success = data.success;
+			if (success){
+				window.location.replace("/webpages/home.jsp");
+			} else {
+				alert("logout failed");
+			}
+		});
+	});
 
 });
