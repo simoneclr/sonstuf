7ad0d3eb253791bcac17a40020a64cf6fabdb7fa
Enrico Testori <hypertesto@gmail.com>
2015-12-10 11:13:50 +0100

    Aggiunte categorie

---
+++ src/main/resources/db/migration/V013__Insert_categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO `sonstuf`.`category` (`name`, `description`) VALUES ('assistenza anziani', 'aiuto e assistenza ad anziani e familiari');
+INSERT INTO `sonstuf`.`category` (`name`, `description`) VALUES ('pulizie', 'aiuto nelle pulizie domestiche');
+INSERT INTO `sonstuf`.`category` (`name`, `description`) VALUES ('studio', 'ripetizioni, aiuto e approfondimento di materie varie');
+INSERT INTO `sonstuf`.`category` (`name`, `description`) VALUES ('fornelli', 'aiuto in cucina');
